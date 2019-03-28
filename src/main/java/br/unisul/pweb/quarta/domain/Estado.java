a17cd50db751247b7d082f8b128586bd6ccfa899
VBViniciusVB <viniciusvianalaguna@hotmail.com>
2019-03-27 21:49:17 -0300

    Adicionado Estados

--- src/main/java/br/unisul/pweb/quarta/domain/Estado.java
+++ src/main/java/br/unisul/pweb/quarta/domain/Estado.java
@@ -8,10 +8,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class Categoria implements Serializable{
+public class Estado implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
@@ -21,28 +23,25 @@ public class Categoria implements Serializable{
 	
 	private String nome;
 
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@JsonIgnore
+	@OneToMany(mappedBy="estado")
+	private List<Cidade> cidades = new ArrayList<>();
 	
-	public Categoria() {
+	
+	
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
+	}
+
+	public Estado() {
 		
 	}
 	
-	
-	
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	}
-
-
-
-	public Categoria(Integer id, String nome) {
+	public Estado(Integer id, String nome) {
 		this.id = id;
 		this.nome = nome;
 	}
@@ -76,7 +75,7 @@ public class Categoria implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Estado other = (Estado) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
